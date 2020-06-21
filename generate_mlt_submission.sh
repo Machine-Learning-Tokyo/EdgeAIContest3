3b7b4eb6b7a3bd5a0db61bd4e920c9102b24a2c4
Shinde, Yoovraj <yashinde@gmail.com>
2020-06-21 19:39:51 +0900

    Updated for new model

--- generate_mlt_submission.sh
+++ generate_mlt_submission.sh
@@ -9,7 +9,7 @@ ls -la mlt_submission/requirements.txt
 
 # copy model file
 mkdir -pv mlt_submission/model
-cp -v /ext/signate_edge_ai/model/resnet50_csv_01.h5.frozen mlt_submission/model/
+cp -v /ext/signate_edge_ai/model/resnet50_csv_06.h5.frozen mlt_submission/model/
 
 # copy source files
 cp -v src/main.py                           mlt_submission/src/
