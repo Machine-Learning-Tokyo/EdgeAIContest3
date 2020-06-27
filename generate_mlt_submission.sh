cc5735698ad4b9d3792120f6ffbc4da9238f1575
Shinde, Yoovraj <yashinde@gmail.com>
2020-06-27 17:11:43 +0900

    Updated script for using latest model resnet101_csv_15.5classes.all_bboxes.h5.frozen

--- generate_mlt_submission.sh
+++ generate_mlt_submission.sh
@@ -9,7 +9,7 @@ ls -la mlt_submission/requirements.txt
 
 # copy model file
 mkdir -pv mlt_submission/model
-cp -v /ext/signate_edge_ai/model/resnet50_csv_06.h5.frozen mlt_submission/model/
+cp -v /ext/signate_edge_ai/model/resnet101_csv_15.5classes.all_bboxes.h5.frozen mlt_submission/model/
 
 # copy source files
 cp -v src/main.py                           mlt_submission/src/
--- test_submit.sh
+++ test_submit.sh
@@ -12,6 +12,6 @@ cp -v src/main.py                           sample_submit/src/
 
 # copy model file
 mkdir -pv sample_submit/model
-cp -v /ext/signate_edge_ai/model/resnet50_csv_06.h5.frozen sample_submit/model/
+cp -v /ext/signate_edge_ai/model/resnet101_csv_15.5classes.all_bboxes.h5.frozen sample_submit/model/
 
 zip -q -r sample_submit.zip sample_submit
