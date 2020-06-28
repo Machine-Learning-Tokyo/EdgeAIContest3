dc7f87b3ec0f85efe50a79b1e27b2a4786cb6dc9
Shinde, Yoovraj <yashinde@gmail.com>
2020-06-29 00:07:43 +0900

    Updated script to detect and delete existing file if present to create new fresh file

--- generate_mlt_submission.sh
+++ generate_mlt_submission.sh
@@ -15,3 +15,8 @@ cp -v /ext/signate_edge_ai/model/resnet101_csv_15.5classes.all_bboxes.h5.frozen 
 cp -v src/main.py                           mlt_submission/src/
 cp -v src/predictor.py                      mlt_submission/src/
 cp -v src/object_tracker.py                 mlt_submission/src/
+
+if [ -f mlt_submission.zip ]; then
+    rm mlt_submission.zip
+    zip -q -r mlt_submission_`date +"%m%d%H%m"`.zip mlt_submission
+fi
